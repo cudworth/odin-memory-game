692442b2a31587590717ef2acd6b67076bf82f89
cudworth <austincudworth@gmail.com>
2022-01-16 22:33:37 -0800

    Misc revisions

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+import { useState } from "react";
+import Header from "./components/Header";
+import Game from "./components/Game";
+
+function App(props) {
+  const [state, setState] = useState({ gameScore: 0, highScore: 0 });
+
+  function handleScore() {
+    setState((prev) => {
+      const next = { ...prev };
+      next.gameScore = prev.gameScore + 1;
+      return next;
+    });
+  }
+
+  function handleGameEnd() {
+    alert(`Game score: ${state.gameScore}`);
+    if (state.highScore < state.gameScore) {
+      setState((prev) => {
+        const next = { ...prev };
+        next.highScore = prev.gameScore;
+        next.gameScore = 0;
+        return next;
+      });
+    } else {
+      setState((prev) => {
+        const next = { ...prev };
+        next.highScore = prev.highScore;
+        next.gameScore = 0;
+        return next;
+      });
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header highScore={state.highScore} gameScore={state.gameScore} />
+      <Game onGameEnd={handleGameEnd} onScore={handleScore} />
     </div>
   );
 }
